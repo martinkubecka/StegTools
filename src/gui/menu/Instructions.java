991fe03e5b61380a3a8fe73250d059e03576c8c3
Martin <martinkubecka@protonmail.com>
2020-11-14 12:31:02 +0100

    GUI Set up

---
+++ src/gui/menu/Instructions.java
@@ -0,0 +1,53 @@
+package gui.menu;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Instructions extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+
+    public Instructions() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    /*public static void main(String[] args) {
+        Instructions dialog = new Instructions();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }*/
+}
